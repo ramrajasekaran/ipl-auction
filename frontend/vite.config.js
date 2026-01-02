c6871a995da6b7e8f7211b4dcc5cfbc085b1c64c
ramrajasekaran <sriramsriram16145@gmail.com>
2026-01-02 23:04:49 +0530

    redirects

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    define: {
+        // ⚠️ REPLACE THIS WITH YOUR HOSTED BACKEND URL
+        'import.meta.env.VITE_API_BASE': JSON.stringify('https://ipl-auction-8a8o.onrender.com/api'),
+    },
     server: {
         port: 5173,
         proxy: {
