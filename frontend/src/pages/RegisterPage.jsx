4dbc7afbca1b30bfc21cf4253127a859ae75d21f
ramrajasekaran <sriramsriram16145@gmail.com>
2026-01-03 23:40:42 +0530

    Commit

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -146,6 +146,11 @@ const RegisterPage = () => {
                                     required
                                 />
                             </div>
+                            <div className="mt-2 text-right">
+                                <Link to="/reset-password" className="text-sm text-green-500 hover:underline">
+                                    Forgot Password?
+                                </Link>
+                            </div>
                         </div>
 
                         {error && (
